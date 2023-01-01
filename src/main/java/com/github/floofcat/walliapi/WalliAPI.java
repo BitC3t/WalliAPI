eb26d947e289c9af8b5b511993d8dea084ab8f0c
Floof <floofywastaken@gmail.com>
2023-01-02 01:32:27 +0530

    [F] Initial Commit

---
+++ src/main/java/com/github/floofcat/walliapi/WalliAPI.java
@@ -0,0 +1,17 @@
+package com.github.floofcat.walliapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WalliAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
