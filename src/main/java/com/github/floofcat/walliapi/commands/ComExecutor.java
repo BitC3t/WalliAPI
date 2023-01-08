6cc278afaf295c2fe3a991c44fed9e6586b423da
Floof <floofywastaken@gmail.com>
2023-01-08 16:15:12 +0530

    [F] Commands

--- src/main/java/com/github/floofcat/walliapi/commands/ComExecutor.java
+++ src/main/java/com/github/floofcat/walliapi/commands/ComExecutor.java
@@ -5,13 +5,18 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class ComExecutor implements CommandExecutor {
+public class ComExecutor implements CommandExecutor, TabCompleter {
 
     private final WalliAPI plugin;
     private List<String> commands = Arrays.asList("set-team");
@@ -44,5 +49,25 @@ public class ComExecutor implements CommandExecutor {
                 break;
 
         }
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        final List<String> completions = new ArrayList<>();
+
+        if(args.length == 1) {
+            completions.clear();
+            List<String> tempList = new ArrayList<>();
+
+            for(String c : commands) {
+                tempList.add(c);
+            }
+
+            StringUtil.copyPartialMatches(args[0], tempList, completions);
+            Collections.sort(completions);
+
+            return completions;
+        }
+        return completions;
     }
 }
---
+++ src/main/java/com/github/floofcat/walliapi/leaderboards/Leaderboard.java
@@ -0,0 +1,4 @@
+package com.github.floofcat.walliapi.leaderboards;
+
+public class Leaderboard {
+}
